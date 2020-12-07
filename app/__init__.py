dbfcdb053568fb63f0d48226b18f4aefec98d4a1
flecmart <flecmart@gmail.com>
2020-12-07 15:13:05 +0100

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import flask_sqlalchemy
+
+from .models import db
+from . import config
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.app_context().push()
+    db.init_app(app)
+    db.create_all()
+    return app
