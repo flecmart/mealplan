1b3320654abb79cdea4e65be86bf6739769b23e7
flecmart <flecmart@gmail.com>
2020-12-07 15:41:42 +0100

    update

--- app/config.py
+++ app/config.py
@@ -6,4 +6,11 @@ host = os.environ['POSTGRES_HOST']
 database = os.environ['POSTGRES_DB']
 port = os.environ['POSTGRES_PORT']
 
+# for local testing
+#user = 'test'
+#password = 'password'
+#host= 'postgress'
+#port=5432
+#database='example'
+
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
