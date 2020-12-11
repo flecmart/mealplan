a9c4b05ebca35ab459c503a0bd4cdd2e305905bc
flecmart <flecmart@gmail.com>
2020-12-11 16:59:13 +0100

    fix connection between db and app

--- app/config.py
+++ app/config.py
@@ -1,16 +1,9 @@
 import os
 
 user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRESS_PASSWORD']
+password = os.environ['POSTGRES_PASSWORD']
 host = os.environ['POSTGRES_HOST']
 database = os.environ['POSTGRES_DB']
 port = os.environ['POSTGRES_PORT']
-
-# for local testing
-#user = 'test'
-#password = 'password'
-#host= 'postgress'
-#port=5432
-#database='example'
 
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
