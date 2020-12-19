181c3939dd53bb56e9918ace17e85516a6daf2fe
flecmart <flecmart@gmail.com>
2020-12-19 16:29:55 +0100

    sketch db model

--- app/app.py
+++ app/app.py
@@ -1,25 +1,54 @@
 import json
 
-from flask import request
+from flask import request, flash
 
 from . import create_app
-from .models import db, Cats
+from . import database
+from .models import db, Recipe, PlannedMeal
 
 app = create_app()
+IMG_FOLDER = '../img'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 @app.route('/', methods=['GET'])
-def fetch():
-    cats = Cats.query.all()
-    all_cats = []
-    for cat in cats:
-        new_cat = {
-            "id": cat.id,
-            "name": cat.name,
-            "price": cat.price,
-            "breed": cat.breed
+def show_data():
+    recipes = database.query_all(Recipe)
+    recipe_json = []
+    for recipe in recipes:
+        new_recipe = {
+            "id": recipe.id,
+            "name": recipe.name,
+            "ingredients": recipe.ingredients,
+            "instructions": recipe.instructions,
+            "time": recipe.time,
+            "image_path": recipe.image_path
         }
+        recipe_json.append(new_recipe)
+    return json.dumps(recipe_json), 200
+    
+@app.route('/add_recipe', methods=['POST'])
+def save_recipe():
+    name = request.form['name']
+    time = request.form['time']
+    ingredients = request.form['ingredients']
+    instructions = request.form['instructions']
+    image_path = '' # TODO: define default image
+    # TODO: request image_path
+    # e.g. upload img files: https://stackoverflow.com/questions/44926465/upload-image-in-flask
+    # it should also be able to add images to recipes later on, edit recipes in general
 
-        all_cats.append(new_cat)
-    return json.dumps(all_cats), 200
+    if time == '': 
+        time = 30 # default in min
 
-# todo create routes for data examples
+    if image_path == '':
+        image_path = 'default'
+
+    if len(name) == 0 or len(ingredients) == 0 or len(instructions) == 0:
+        flash('Please fill in Name, Ingredients, and Instructions.', 'negative')
+
+    same_recipe = Recipe.query.filter_by(name=name, instructions=instructions, cookbook_id=c_book.id).first()
+    if same_recipe:
+        flash(f'The recipe {name} already exists.', 'negative')
+
+    database.add_instance(Recipe(name, str(ingredients), instructions, str(time), str(image_path)))
+    flash(f'Recipe {name} saved.', 'positive')
--- app/models.py
+++ app/models.py
@@ -2,9 +2,30 @@ import flask_sqlalchemy
 
 db = flask_sqlalchemy.SQLAlchemy()
 
-class Cats(db.Model):
-    __tablename__ = 'cats'
+class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    price = db.Column(db.Integer)
-    breed = db.Column(db.String(100))
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False) # comma separated
+    instructions = db.Column(db.Text, nullable=False) # free text vs csv? TODO: Format text
+    time = db.Column(db.Integer, nullable=True) # time in minutes
+    image_path = db.Column(db.String(255), nullable=True) # path to potential image in filesystem? TODO: new container mount?
+    __table_args__ = (db.UniqueConstraint('name', name='_uc_recipe_name'),)
+    
+    def __init__(self, name, ingredients, instructions, time, image_path):
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.time = time
+        self.image_path = image_path
+
+    def get_ingredients_list(self):
+        return self.ingredients.split(',')
+
+class PlannedMeal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fk_recipe = db.Column(db.Integer, db.ForeignKey('recipe.id')) 
+    date = db.Column(db.Date)
+
+    def __init__(self, fk_recipe, date):
+        self.fk_recipe = fk_recipe
+        self.date = date
