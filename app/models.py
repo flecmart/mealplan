974253c797b9613db31bc4b1ce83ce65392891cf
flecmart <flecmart@gmail.com>
2021-03-16 11:24:27 +0100

    Ignore case on abbreviations

--- app/models.py
+++ app/models.py
@@ -32,7 +32,7 @@ class Recipe(db.Model):
         instructions = re.sub('<.*?>', '', instructions)
 
         # splits string at "." and ignore abbreviations.
-        instructions = re.split(r'(?<!ca)(?<!evtl)\.', instructions)
+        instructions = re.split(r'(?<!ca)(?<!evtl)\.', instructions, flags=re.IGNORECASE)
 
         # handles cases where parentheses exist in instructions
         fresh_instructions = []
